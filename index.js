47b42a73ff68e1fffe03df441933a5d191ddba0e
annzhzho@gmail.com <annzhzho@gmail.com>
2023-06-02 18:47:17 +0300

    fix files

--- index.js
+++ index.js
@@ -5,9 +5,8 @@ const userText = document.querySelector("#user-text");
 function modifyUserName(str) {
   if (!str) {
     return str;
-  } else {
-    return (str[0].toUpperCase() + str.slice(1).toLowerCase()).trim();
   }
+  return (str[0].toUpperCase() + str.slice(1).toLowerCase()).trim();
 }
 
 const commentAvatar = document.querySelector(".comment__avatar");
@@ -19,15 +18,35 @@ function showAvatar(src) {
 }
 
 function checkSpam(str) {
-  str = str.replace(/(viagra|XXX)/gi, "***");
-  return str;
+  return str.replace(/(viagra|XXX)/gi, "***");
 }
 
 const button = document.querySelector(".button");
 const commentName = document.querySelector(".comment__name");
 const commentText = document.querySelector(".comment__text");
-const plugName = "Anonimus";
-const plugAvatar = "./assets/images/avatar_null.jpg";
+const plugName = "username";
+
+//аватарки
+const arrayAvatars = [
+  "./assets/images/avatar_1.jpg",
+  "./assets/images/avatar_2.jpg",
+  "./assets/images/avatar_3.jpg",
+  "./assets/images/avatar_4.jpg",
+  "./assets/images/avatar_5.jpg",
+];
+const randomAvatar =
+  arrayAvatars[Math.floor(Math.random() * arrayAvatars.length)];
+
+//время
+const postingTime = document.querySelector("#time");
+const date = new Date();
+const options = {
+  year: "numeric",
+  month: "numeric",
+  day: "numeric",
+  hour: "numeric",
+  minute: "numeric",
+};
 
 button.addEventListener("click", (event) => {
   event.preventDefault();
@@ -37,22 +56,20 @@ button.addEventListener("click", (event) => {
   } else {
     commentName.textContent = modifyUserName(userName.value);
   }
-  userName.value = null;
 
   if (userLink.value === "") {
     let image = document.createElement("img");
-    image.src = plugAvatar;
+    image.src = randomAvatar;
     commentAvatar.append(image);
   } else {
     showAvatar(userLink.value);
   }
-  userLink.value = null;
 
   if (userText.value === "") {
     commentText.value = "Я люблю котиков";
   } else {
     commentText.textContent = checkSpam(userText.value);
   }
-
-  userText.value = null;
+  postingTime.textContent = date.toLocaleString("ru", options);
+  document.querySelector(".user-data").reset();
 });
--- task_star.js
+++
@@ -1,7 +0,0 @@
-function deleteTags(str) {
-  let result = str;
-  result = result.replace(/<(.)*?>/g, "");
-  return result;
-}
-const container = document.querySelector(".container").innerHTML;
-console.log(deleteTags(container));
